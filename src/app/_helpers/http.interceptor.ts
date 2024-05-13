a6d5ad78e55d8c6f5bd509e4535976a104371da5
Chetan Godhani <cdgodhani@gmail.com>
2024-05-13 23:29:30 +0530

    Updates

--- src/app/_helpers/http.interceptor.ts
+++ src/app/_helpers/http.interceptor.ts
@@ -1,20 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { StorageService } from '../_services/storage.service';
 
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
-
-  constructor(private storageService:StorageService){
-
-  }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const currentUser = this.storageService.getUser();
-
     req = req.clone({
       withCredentials: true,
-      headers: req.headers.set('authorization', currentUser?.token)
     });
 
     return next.handle(req);
